eb5620bf0c6d9f29dc74f69c58339767ca5fcfa0
ianfleon <ianfleon08@gmail.com>
2023-06-09 00:49:46 +0900

    delete old file and make new class for all endpoint

--- api/endpoint/add.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// $nama = $_POST['nama'];
-// $posisi = $_POST['posisi'];
-// $foto = $_FILES['foto'];
-
-var_dump($_POST);
--- api/index.php
+++ api/index.php
@@ -1,24 +1,107 @@
 <?php
 
-function api_response($ep)
-{
-    if (!file_exists('endpoint/' . $ep . '.php')) {
-        return json_encode([
-            'status' => 404,
-            'message' => 'API tidak ditemukan'
-        ]);
+require_once('../vendor/autoload.php');
 
-    }
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
+use Endroid\QrCode\Label\Alignment\LabelAlignmentCenter;
+use Endroid\QrCode\Label\Font\NotoSans;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
+use Endroid\QrCode\Writer\PngWriter;
 
-    $db = new SQLite3('../db/absenky.db');
+if (isset($_GET['ep'])) {
 
-    require_once('endpoint/' . $ep . '.php');
+    $api = new AbsenkyAPI();
 
-    
+    $params = [];
+    $method = $_GET['ep'];
+
+    call_user_func_array([$api, $method], $params);
 
 }
 
-if (isset($_GET['q'])) {
-    $ep = $_GET['q'];
-    echo api_response($ep);
+class AbsenkyAPI
+{
+    private $db;
+
+    function __construct()
+    {
+        $this->db = new SQLite3('../db/absenky.db');
+    }
+
+    private function _response($res)
+    {
+        echo json_encode($res);
+        exit;
+    }
+
+    function _upload_foto($foto, $nama = 'foto')
+    {
+
+        // Ekstensi foto yang boleh diinput
+        $valid_extensions = ['jpg', 'jpeg', 'JPG', 'JPEG', 'PNG', 'png', 'webp', 'svg'];
+
+        // Mengambil ekstensi dari nama file
+        $foto_extension = explode('.', $foto['name']);
+        $foto_extension = end($foto_extension);
+
+        // Validasi Ekstensi
+        if (!in_array($foto_extension, $valid_extensions)) {
+            $this->_response([
+                'status' => 500,
+                'messages' => 'Ekstensi foto tidak valid'
+            ]);
+        }
+
+        // Validasi ukuran file foto
+        if ($foto['size'] > 2000000) {
+            $this->_response([
+                'status' => 500,
+                'messages' => 'Ukuran foto tidak boleh > 2MB'
+            ]);
+        }
+
+        // Membuat nama file baru untuk foto
+        $nama_foto_baru = md5($nama) . '.' . $foto_extension;
+
+        // Memindahkan file yang diupload
+        if (!move_uploaded_file($foto['tmp_name'], '../public/foto/' . $nama_foto_baru)) {
+            $this->_response([
+                'status' => 500,
+                'messages' => 'Gagal upload foto'
+            ]);
+        }
+
+        return $nama_foto_baru;
+
+    }
+
+    function add()
+    {
+        // Ambil data yang diambil melalui POST
+        $id = uniqid(time());
+
+        $nama = $_POST['nama'];
+        $posisi = $_POST['posisi'];
+
+        $foto = $this->_upload_foto($_FILES['foto'], $nama);
+
+        $this->db->querySingle("INSERT INTO users_tb VALUES ('$id', '$nama', '$posisi', '$foto')");
+        $this->_generate_qr($id);
+
+    }
+    
+    private function _generate_qr($isi)
+    {
+
+        require_once('../libs/phpqrcode/qrlib.php');
+
+        if (!is_dir('../public/qr')) {
+            mkdir('../public/qr');
+        }
+
+        QRcode::png($isi, '../public/qr/' . $isi . '.png', QR_ECLEVEL_H, 24);
+
+    }
 }
