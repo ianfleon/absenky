98e9c3b1eb6d582fc5cf0e20640ca7574e3994b9
ianfleon <ianfleon08@gmail.com>
2023-06-26 02:57:03 +0900

    form to add and edit staff data

---
+++ form-staff.php
@@ -0,0 +1,146 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['ADMIN_LOGINED'])) {
+    header('Location: /login.php');
+}
+
+if (isset($_GET['idstaff'])) {
+
+    require_once 'api.php';
+    $api = new AbsenkyAPI();
+
+    $staff = $api->getDataById('staffs_tb', $_GET['idstaff']);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require_once('components/head.php'); ?>
+    <title>Tambah User - Absenky</title>
+</head>
+
+<body>
+
+    <div class="container mt-4">
+        <div class="d-flex">
+            <a href="dashboard.php" class="btn btn-light border">Back</a>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="col-lg-6 m-auto">
+
+            <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                </symbol>
+                <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+                </symbol>
+                <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                </symbol>
+            </svg>
+
+            <div class="card">
+                <form action="" id="form_add">
+                    <div class="d-flex align-items-center bg-light px-4 py-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="d-block me-2" width="24" viewBox="0 0 24 24" id="add-account">
+                            <path d="M21,10.5H20v-1a1,1,0,0,0-2,0v1H17a1,1,0,0,0,0,2h1v1a1,1,0,0,0,2,0v-1h1a1,1,0,0,0,0-2Zm-7.7,1.72A4.92,4.92,0,0,0,15,8.5a5,5,0,0,0-10,0,4.92,4.92,0,0,0,1.7,3.72A8,8,0,0,0,2,19.5a1,1,0,0,0,2,0,6,6,0,0,1,12,0,1,1,0,0,0,2,0A8,8,0,0,0,13.3,12.22ZM10,11.5a3,3,0,1,1,3-3A3,3,0,0,1,10,11.5Z"></path>
+                        </svg>
+                        <p class="fw-bold m-0 text-muted">
+                            Tambah User
+                        </p>
+                        <button class="btn btn-primary ms-auto" id="btn_add">Submit</button>
+                    </div>
+                    <div class="p-4">
+                        <div class="mb-3">
+                            <label for="nama" class="form-label">Nama Lengkap</label>
+                            <input type="text" class="form-control" value="<?= (isset($staff)) ? $staff['nama_staff'] : ''; ?>" name="nama_staff" id="nama_staff">
+                        </div>
+                        <div class="mb-3">
+                            <label for="posisi" class="form-label">Posisi</label>
+                            <input type="text" class="form-control" value="<?= (isset($staff)) ? $staff['posisi_staff'] : ''; ?>" name="posisi_staff" id="posisi_staff">
+                        </div>
+                        <div class="mb-3">
+                            <label for="foto" class="form-label">Foto</label>
+                            <?php if (isset($staff)) : ?>
+                                <input type="hidden" name="id_staff" value="<?= $staff['id_staff']; ?>" />
+                                <input type="hidden" name="foto_staff" value="<?= $staff['foto_staff']; ?>" />
+                                <a href="">(<?= $staff['foto_staff']; ?>)</a>
+                            <?php endif; ?>
+                            <input class="form-control" type="file" name="foto_staff" id="foto_staff">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script src="/assets/js/jquery.js"></script>
+
+    <script>
+        const BtnAdd = $('#btn_add').click(function(btn) {
+
+            btn.preventDefault();
+
+            const FormAdd = new FormData(document.getElementById('form_add'));
+
+            let EPURL = 'api.php?ep=add_staff';
+
+            <?php if (isset($staff)) : ?>
+                EPURL = 'api.php?ep=edit_staff';
+            <?php endif; ?>
+
+            $.ajax({
+                method: 'POST',
+                url: EPURL,
+                cache: false,
+                processData: false,
+                contentType: false,
+                data: FormAdd,
+                success: function(res) {
+
+                    $('#form_add').trigger('reset');
+
+                    res = JSON.parse(res);
+
+                    console.log(res);
+
+                    let txtAlert, iconAlert;
+
+                    if (res.status == 200) {
+                        txtAlert = 'BERHASIL';
+                        iconAlert = 'success';
+                    } else {
+                        txtAlert = 'ERROR';
+                        iconAlert = 'error';
+                    }
+
+                    swal({
+                        title: res.message,
+                        text: txtAlert,
+                        icon: iconAlert,
+                        button: "OK",
+                    });
+
+                    setTimeout(() => {
+                        location.reload();
+                    }, 2000);
+                }
+            });
+        });
+    </script>
+
+    <script>
+
+    </script>
+
+</body>
+
+</html>
